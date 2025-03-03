d73cac296d0a9652c7a989fcc79408759c13eebd
omanaokar <omanaokar@gmail.com>
2025-03-04 00:51:30 +0530

    Om's commits

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
 const express = require('express')
-const { MongoClient } = require('mongodb');
+const mongoose = require('mongoose');
+const path = require("path");
 const User = require('./models/User')
-
-const uri = "mongodb+srv://burhanuddin:burhanuddin@cluster0.1fngs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-const client = new MongoClient(uri);
 
 const app = express()
 
-async function run() {
-    try {
-      await client.connect();
-      const db = client.db('Library');
-      const collection = db.collection('users');
-  
-      // Find the first document in the collection
-      console.log("Database connected")
-    } finally {
-      // Close the database connection when finished or an error occurs
-      await client.close();
-    }
-  }
-  run().catch(console.error);
+mongoose.connect("mongodb://127.0.0.1:27017/foeprojects", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 10000, // 10 seconds timeout
+})
+.then(() => console.log("✅ MongoDB Connected!"))
+.catch(err => console.error("❌ MongoDB Connection Failed:", err));
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
 app.engine('html', require('ejs').renderFile);
 
@@ -28,12 +22,19 @@ app.get('/', (req, res)=>{
     res.render(__dirname+'/index.html')
 })
 
-app.post('/create', async (req, res) => {
+app.get('/login', (req, res)=>{
+  res.render(__dirname+'/login.html')
+})
+
+app.post('/api/createuser', async (req, res) => {
     try{
-        const newUser = new User(req.body);
+        console.log(req.body)
+        const { fname, lname, email, password } = req.body;
+        const newUser = new User({
+          fname, lname, email, password
+        });
         const savedUser = await newUser.save(); 
-        console.log("user created!")
-        res.status(201).json(savedUser);
+        res.status(201).json({ message: "User created successfully!", user: newUser });
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
